606521b31803aadbcbef83f403bbcab48fa86adc
Jone <jshells97@yandex.ru>
2023-02-07 10:21:27 +0300

    create_migration_and_model

---
+++ database/migrations/2023_02_07_064940_create_categories_goods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_goods', function (Blueprint $table) {
+            $table->id();
+            $table->string('shoes');
+            $table->string('clothes');
+            $table->string('headdress');
+            $table->string('accessories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories_goods');
+    }
+};
---
+++ database/migrations/2023_02_07_065155_create_child_goods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('child_goods', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_child');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('child_goods');
+    }
+};
